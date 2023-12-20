48117ad11b318baa268a99aa554e1bee572fe7bc
aniketpethe07 <aniketpethe07@gmail.com>
2023-12-20 23:21:34 +0530

    Completed 3rd tutorial

--- Tut3.js
+++ Tut3.js
@@ -3,3 +3,47 @@
 //for loop
 //while loop
 //do-while loop
+
+//for-of loop //used for array and strings
+let str1 = "HarryPotter";
+for(let val of str1){
+    console.log(val);
+}
+
+//for-in loop
+let vigilante = {
+    name : 'Aniket Pethe',
+    age : 22,
+    SGPA : 7.5,
+    isPass : true
+};
+
+for(let key in vigilante){
+    console.log(vigilante[key]);
+}
+
+/* Strings in JS */
+let str2 = 'Harry Potter';
+
+//String inbuild properties
+
+//string.length
+console.log(str2.length);
+console.log(str2[0]);
+
+//Template lirerals
+let obj = {
+    key1 : 'value1',
+    key2 : 'value2',
+    key3 : 'value3',
+};
+console.log(`This is a template literal ${obj.key2}`);
+
+//String methods
+
+let str3 = '    HarryPotter    ';
+console.log(str3.toLowerCase());
+console.log(str3.toUpperCase());
+console.log(str3.trim());
+console.log(str3.slice(3, 9));
+console.log(str3.replace('Harry', 'James'));
